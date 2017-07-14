efafa06ad238cf24b679c55d510fa657a886774b
Steven <lilianbo1975@gmail.com>
2017-07-14 08:50:29 -0700

    delete no use ld file

---
+++ build/gcc/startup_LPC54608.d
@@ -0,0 +1,3 @@
+gcc/startup_LPC54608.o: \
+ ../sdk/SDK_2.2_LPCXpresso54608/devices/LPC54608/mcuxpresso/startup_LPC54608.c \
+ ../inc/crp.h
---
+++ build/gcc/system_LPC54608.d
@@ -0,0 +1,18 @@
+gcc/system_LPC54608.o: \
+ ../sdk/SDK_2.2_LPCXpresso54608/devices/LPC54608/system_LPC54608.c \
+ /home/steven/projects/FreeRTOS_OpenOCD/tools/gcc-arm-none-eabi-6-2017-q2-update/lib/gcc/arm-none-eabi/6.3.1/include/stdint.h \
+ /home/steven/projects/FreeRTOS_OpenOCD/tools/gcc-arm-none-eabi-6-2017-q2-update/arm-none-eabi/include/stdint.h \
+ /home/steven/projects/FreeRTOS_OpenOCD/tools/gcc-arm-none-eabi-6-2017-q2-update/arm-none-eabi/include/machine/_default_types.h \
+ /home/steven/projects/FreeRTOS_OpenOCD/tools/gcc-arm-none-eabi-6-2017-q2-update/arm-none-eabi/include/sys/features.h \
+ /home/steven/projects/FreeRTOS_OpenOCD/tools/gcc-arm-none-eabi-6-2017-q2-update/arm-none-eabi/include/_newlib_version.h \
+ /home/steven/projects/FreeRTOS_OpenOCD/tools/gcc-arm-none-eabi-6-2017-q2-update/arm-none-eabi/include/sys/_intsup.h \
+ /home/steven/projects/FreeRTOS_OpenOCD/tools/gcc-arm-none-eabi-6-2017-q2-update/arm-none-eabi/include/sys/_stdint.h \
+ ../sdk/SDK_2.2_LPCXpresso54608/devices/LPC54608/fsl_device_registers.h \
+ ../sdk/SDK_2.2_LPCXpresso54608/devices/LPC54608/LPC54608.h \
+ ../sdk/SDK_2.2_LPCXpresso54608/CMSIS/Include/core_cm4.h \
+ ../sdk/SDK_2.2_LPCXpresso54608/CMSIS/Include/core_cmInstr.h \
+ ../sdk/SDK_2.2_LPCXpresso54608/CMSIS/Include/cmsis_gcc.h \
+ ../sdk/SDK_2.2_LPCXpresso54608/CMSIS/Include/core_cmFunc.h \
+ ../sdk/SDK_2.2_LPCXpresso54608/CMSIS/Include/core_cmSimd.h \
+ ../sdk/SDK_2.2_LPCXpresso54608/devices/LPC54608/system_LPC54608.h \
+ ../sdk/SDK_2.2_LPCXpresso54608/devices/LPC54608/LPC54608_features.h
